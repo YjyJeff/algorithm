0d2ebc99b21670db267ade1999b90faf52246ede
Jeff Yang <1731939194@qq.com>
2016-11-01 00:22:08 +0800

    fix bug

--- data_structure/dynamic_array/DynamicArray.h
+++ data_structure/dynamic_array/DynamicArray.h
@@ -20,8 +20,6 @@ public:
 };
 
 
-
-
 template <typename Type >
 DynamicArray<Type>::DynamicArray(){
   Type* temp = new Type[2];
@@ -39,7 +37,7 @@ template <typename Type >
 Type DynamicArray<Type>::Get( int i ){
   if (i < 0 && i > this->size_){
     cout << "Index error !" << endl;
-    return ;
+    exit(-1);
   }
   return this->parray_[i];
 }
@@ -58,15 +56,15 @@ template <typename Type >
 void DynamicArray<Type>::PushBack( Type value ){
   if (this->size_ == this->capacity_){
     Type* temp = new Type[2 * this->capacity_];
-    for(int i=0; i<this->capacity_; i++)
+    for(int i=0; i < this->capacity_; i++){
       temp[i] = this->parray_[i];
+    }
     delete [] this->parray_;
     this->parray_ = temp;
+    this->capacity_ *= 2;
   }
-
   this->parray_[this->size_] = value;
   this->size_ += 1;
-  this->capacity_ *= 2;
   return ;
 }
 
@@ -79,12 +77,14 @@ bool DynamicArray<Type>::Remove( int i ){
 
   for(int j=i+1; j<this->size_; j++)
     this->parray_[j-1] = this->parray_[j];
+  this->size_ -= 1;
   return true;
 }
 
 template <typename Type >
 DynamicArray<Type>::~DynamicArray(){
   delete [] this->parray_;
+
 }
 
 
--- data_structure/dynamic_array/main.cpp
+++ data_structure/dynamic_array/main.cpp
@@ -1,10 +1,14 @@
 #include "DynamicArray.h"
+#include "iostream"
 
 int main(){
 
-  DynamicArray<int>* arry = new DynamicArray<int>();
+  DynamicArray<int> arry;
   for(int i=0; i<20;i++)
-    arry->PushBack(i);
-  delete arry;
+    arry.PushBack(i);
+  arry.Remove(9);
+  for(int i=0; i<arry.Size();i++)
+    std::cout << arry.Get(i) << " ";
+  std::cout << endl;
   return 0;
 }
